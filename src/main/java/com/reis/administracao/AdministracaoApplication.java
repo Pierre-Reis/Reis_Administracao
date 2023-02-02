b14bef24fbb0378c0ed002c4bfa9ac50e48fbe63
Pierre Reis <pierre.developer@hotmail.com>
2023-02-02 08:25:25 -0300

    Projeto Criado

---
+++ src/main/java/com/reis/administracao/AdministracaoApplication.java
@@ -0,0 +1,13 @@
+package com.reis.administracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracaoApplication.class, args);
+	}
+
+}
